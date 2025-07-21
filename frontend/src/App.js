50912b91b18bf61dc5f17b38a03effe695c0bd87
mugunth-maker <mugunthanbalaji2005@gmail.com>
2025-07-21 22:12:34 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Footer from './components/sections/footer/footer';
 import Navbar from './components/sections/Navbar/Navbar';
-import HeroVideoSection from './components/sections/hero/hero';
 import Contact from './components/sections/contact/contact';
 
 function App() {
   return (
     <>
       <Navbar/>
-      <HeroVideoSection/>
       <Contact/>
       <Footer/> 
     </>
